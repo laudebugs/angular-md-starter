d6c6327f6d94dc4f0b76fe824e422c6e71477e6a
laudebugs <laudebugs@gmail.com>
2023-04-01 01:16:57 +0300

    fix: starter files

--- src/app/core/shared.module.ts
+++ src/app/core/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { MaterialModule } from './material.module'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
+
 @NgModule({
     declarations: [],
-    imports: [MaterialModule, FormsModule, ReactiveFormsModule],
-    exports: [MaterialModule, FormsModule, ReactiveFormsModule],
+    imports: [MaterialModule, FormsModule, ReactiveFormsModule, CommonModule],
+    exports: [MaterialModule, FormsModule, ReactiveFormsModule, CommonModule],
 })
 export class SharedModule {}
