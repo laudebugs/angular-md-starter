d5684724355f7d44ba832bef951e9eae5d69a105
laudebugs <laudebugs@gmail.com>
2023-04-02 14:09:55 +0300

    feat: create demo for using create component options

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import {
-    AfterContentChecked,
     AfterContentInit,
     AfterViewInit,
     ChangeDetectorRef,
     Component,
+    ComponentRef,
+    Injector,
     ViewChild,
     ViewContainerRef,
 } from '@angular/core'
 import { MovieHostDirective } from '../core/directives/movie-host.directive'
 import { MovieComponent } from '../core/components/movie/movie.component'
-import { MOVIES } from '../core/components/movie.data'
+import { IMovie, MOVIES, MovieToken } from '../core/components/movie.data'
+import { MovieWithRequiredDependencyComponent } from '../core/components/movie/movie-with-required-dependency.component'
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms'
 
 @Component({
     selector: 'app-home',
@@ -20,7 +23,17 @@ export class HomeComponent implements AfterViewInit, AfterContentInit {
     /* Look up the element containing the MoviehostDirective */
     @ViewChild(MovieHostDirective, { static: true, read: ViewContainerRef }) movieHost!: ViewContainerRef
 
-    constructor(private hostContainerRef: ViewContainerRef) {}
+    /* Look Up the template reference called TemplateRefAnchor */
+    @ViewChild('TemplateRefAnchor', { static: true, read: ViewContainerRef }) templateRefAnchor!: ViewContainerRef
+
+    /* Another template reference to use createOptions */
+    @ViewChild('UsingCreateComponentOptionsAnchor', { static: true, read: ViewContainerRef })
+    createComponentOptionsAnchor!: ViewContainerRef
+
+    readonly movieList = MOVIES
+    constructor(private hostContainerRef: ViewContainerRef, private parentInjector: Injector, private formBuilder: FormBuilder) {
+        this.createForm()
+    }
 
     /**
      * Note: Avoid directly modifying a components data directly here
@@ -40,23 +53,88 @@ export class HomeComponent implements AfterViewInit, AfterContentInit {
 
         /* Using the directive as an anchor */
         this.useDirectiveAsAnchor()
+
+        /* Using the template reference as an anchor */
+        this.useTemplateRefAsAnchor()
     }
-    ngAfterContentChecked(): void {}
 
     /**
      * Create a new component and attach it to the element containing the directive
      */
     useDirectiveAsAnchor(): void {
         const component = this.movieHost.createComponent(MovieComponent)
+        component.instance.movie = MOVIES[0]
+    }
+
+    useTemplateRefAsAnchor(): void {
+        const component = this.templateRefAnchor.createComponent(MovieComponent)
         component.instance.movie = MOVIES[1]
     }
 
     /**
      * Use host element as anchor for the component
-     * Note: This still throws the ExpressionChangedAfterItHasBeenCheckedError error
+     *
+     * Note: This still throws the [`ExpressionChangedAfterItHasBeenCheckedError`](https://angular.io/errors/NG0100) error
      */
     useHostElementAsAnchor(): void {
         const component = this.hostContainerRef.createComponent(MovieComponent)
-        component.instance.movie = MOVIES[0]
+        component.instance.movie = MOVIES[2]
+    }
+
+    generateComponents() {
+        /* We have to sort here and create the movies in order of index */
+        const movies = this.createComponentsForm.controls.movies.getRawValue().sort((a, b) => a.index - b.index)
+        /* First destroy all previously created components */
+        this.dynamicallyCreatedComponents.forEach((component) => component.destroy())
+
+        /* Create the dynamic components */
+        movies.forEach((movie) => {
+            const injector = Injector.create({
+                providers: [
+                    {
+                        provide: MovieToken,
+                        useValue: movie.movie,
+                    },
+                ],
+                parent: this.parentInjector,
+            })
+            const component = this.createComponentOptionsAnchor.createComponent(MovieWithRequiredDependencyComponent, {
+                injector,
+                index: movie.index,
+            })
+            this.dynamicallyCreatedComponents.push(component)
+        })
+    }
+
+    private dynamicallyCreatedComponents: Array<ComponentRef<MovieWithRequiredDependencyComponent>> = []
+
+    createComponentsForm!: FormGroup<{ movies: FormArray<FormGroup<CreateMovieForm>> }>
+    createForm() {
+        this.createComponentsForm = this.formBuilder.nonNullable.group<{ movies: FormArray<FormGroup<CreateMovieForm>> }>({
+            movies: new FormArray([
+                new FormGroup<CreateMovieForm>({
+                    movie: this.formBuilder.nonNullable.control(MOVIES[0]),
+                    index: this.formBuilder.nonNullable.control(0),
+                }),
+            ]),
+        })
+        this.createComponentsForm.controls.movies
+    }
+
+    addControl() {
+        this.createComponentsForm.controls.movies.push(
+            new FormGroup<CreateMovieForm>({
+                movie: this.formBuilder.nonNullable.control(
+                    this.movieList[this.createComponentsForm.controls.movies.length + (1 % this.movieList.length) - 1],
+                ),
+                index: this.formBuilder.nonNullable.control(this.createComponentsForm.controls.movies.length - 1),
+            }),
+        )
+    }
+
+    removeControl(index: number) {
+        this.createComponentsForm.controls.movies.removeAt(index)
     }
 }
+
+type CreateMovieForm = { movie: FormControl<IMovie>; index: FormControl<number> }
