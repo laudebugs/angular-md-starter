07f36cfbbd098858200d534af9a23c4c678140e0
laudebugs <laudebugs@gmail.com>
2023-04-02 20:43:53 +0300

    chore: organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './core/material.module'
 import { SharedModule } from './core/shared.module'
 import { HomeComponent } from './home/home.component'
-import { MaterialModule } from './core/material.module'
-import { CommonModule } from '@angular/common'
 
 @NgModule({
     declarations: [AppComponent, HomeComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, MaterialModule, CommonModule],
-    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
